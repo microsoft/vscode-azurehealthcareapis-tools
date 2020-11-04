ffee49fe6f1a3d85c2ec35e939cdf4125d2707c1
Yankun Huang <yankhuan@microsoft.com>
2020-11-04 12:14:31 +0800

    Fix result after refreshing

--- FHIR-Converter/client/src/common/conversion.ts
+++ FHIR-Converter/client/src/common/conversion.ts
@@ -51,7 +51,7 @@ export async function fhirConversion(activeDataPath: string, activeTemplatePath:
         const resultFileName = path.join(resultFolder ,dataName + ' - ' + templateName + '.json');
         let exists = fs.existsSync(resultFileName);
         if(!exists){
-            utils.checkFolderWirtePrettyJson(resultFileName, msg);
+            utils.checkFolderWirtePrettyJson(resultFileName, msg.FhirResource);
         }
 
         // show result
