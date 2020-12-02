bd12df00356f0c41518ffddc16df910294e133d2
yankunhuang-pku <66581117+yankunhuang-pku@users.noreply.github.com>
2020-12-02 13:54:36 +0800

    Update the liquid engine (#6)

--- FHIR-Converter/client/src/test/suite/index.ts
+++ FHIR-Converter/client/src/test/suite/index.ts
@@ -46,7 +46,7 @@ export async function run(): Promise<void> {
 	const runner = new Mocha(mochaOpts);
 
 	runner.useColors(true);
-	  
+	runner.addFile(join(__dirname, './view/extension.test'));
 	runner.addFile(join(__dirname, './converter/converter.test'));
 	runner.addFile(join(__dirname, './converter/engine/hl7v2-converter-engine.test'));
 
