91cdf8146021ef006f435ec26ed431d1e983e00b
Yankun Huang <yankhuan@microsoft.com>
2020-12-17 17:04:38 +0800

    Add status bar for converting

--- FHIR-Converter/client/src/view/user-commands/convert.ts
+++ FHIR-Converter/client/src/view/user-commands/convert.ts
@@ -15,52 +15,64 @@ import { showResultEditor } from '../common/editor/show-result-editor';
 import { ConverterEngineFactory } from '../../core/converter/converter-factory';
 
 export async function convertCommand() {
-	// Check whether there is any template not saved and ask if users want to save it
-	const unsavedTemplates: vscode.TextDocument[] = interaction.getUnsavedFiles(engineConstants.TemplateFileExt);
-	if (unsavedTemplates.length > 0) {
-		await interaction.askSaveFiles(unsavedTemplates, localize('message.saveTemplatesBeforeRefresh'), localize('message.save'), localize('message.ignore'));
-	}
+	// Add conversion bar
+	const conversionBar: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	conversionBar.text = '$(sync~spin) Converting...';
+	conversionBar.show();
 
-	// Get the data file
-	const dataFile = globals.settingManager.getWorkspaceState(stateConstants.DataKey);
-
-	// Check whether data file is dirty and not saved and ask if users want to save it
-	const doc = interaction.isDirtyFile(dataFile);
-	if (doc) {
-		await interaction.askSaveFiles([doc], localize('message.saveDataBeforeRefresh'), localize('message.save'), localize('message.ignore'));
-	}
-	
-	// create the converter
-	const converter = ConverterEngineFactory.getInstance().createConverter();
-
-	// Execute the conversion process
-	const resultFile = await converter.convert(dataFile);
-
-	// Open the data in the editor
-	await vscode.window.showTextDocument(vscode.Uri.file(dataFile), {
-		viewColumn: vscode.ViewColumn.One
-	});
-	
-	// Open the template in the editor
-	const templateFile = globals.settingManager.getWorkspaceState(stateConstants.TemplateKey);
-	await vscode.window.showTextDocument(vscode.Uri.file(templateFile), {
-		viewColumn: vscode.ViewColumn.Two,
-	});
-	
-	// Obtain the enableDiffView option from the settings.
-	const enableDiff = globals.settingManager.getWorkspaceConfiguration(configurationConstants.enableDiffViewKey);
-	if (!enableDiff) {
-		await showResultEditor(vscode.Uri.file(resultFile));
-	} else {
-		// Get the history
-		const history = converter.getHistory(resultFile);
-		if (history.length === 1) {
-			// Show result in the editor
-			await showResultEditor(vscode.Uri.file(history[0]));
-		} else if (history.length > 1) {
-			// Show result with the differential view
-			await showDifferentialView(vscode.Uri.file(history[1]), vscode.Uri.file(history[0]));
+	try {
+		// Check whether there is any template not saved and ask if users want to save it
+		const unsavedTemplates: vscode.TextDocument[] = interaction.getUnsavedFiles(engineConstants.TemplateFileExt);
+		if (unsavedTemplates.length > 0) {
+			await interaction.askSaveFiles(unsavedTemplates, localize('message.saveTemplatesBeforeRefresh'), localize('message.save'), localize('message.ignore'));
 		}
+
+		// Get the data file
+		const dataFile = globals.settingManager.getWorkspaceState(stateConstants.DataKey);
+
+		// Check whether data file is dirty and not saved and ask if users want to save it
+		const doc = interaction.isDirtyFile(dataFile);
+		if (doc) {
+			await interaction.askSaveFiles([doc], localize('message.saveDataBeforeRefresh'), localize('message.save'), localize('message.ignore'));
+		}
+		
+		// create the converter
+		const converter = ConverterEngineFactory.getInstance().createConverter();
+
+		// Execute the conversion process
+		const resultFile = await converter.convert(dataFile);
+
+		// Open the data in the editor
+		await vscode.window.showTextDocument(vscode.Uri.file(dataFile), {
+			viewColumn: vscode.ViewColumn.One
+		});
+		
+		// Open the template in the editor
+		const templateFile = globals.settingManager.getWorkspaceState(stateConstants.TemplateKey);
+		await vscode.window.showTextDocument(vscode.Uri.file(templateFile), {
+			viewColumn: vscode.ViewColumn.Two,
+		});
+		
+		// Obtain the enableDiffView option from the settings.
+		const enableDiff = globals.settingManager.getWorkspaceConfiguration(configurationConstants.enableDiffViewKey);
+		if (!enableDiff) {
+			await showResultEditor(vscode.Uri.file(resultFile));
+		} else {
+			// Get the history
+			const history = converter.getHistory(resultFile);
+			if (history.length === 1) {
+				// Show result in the editor
+				await showResultEditor(vscode.Uri.file(history[0]));
+			} else if (history.length > 1) {
+				// Show result with the differential view
+				await showDifferentialView(vscode.Uri.file(history[1]), vscode.Uri.file(history[0]));
+			}
+		}
+		await vscode.commands.executeCommand('workbench.action.closeOtherEditors');
+	} catch (err) {
+		throw err;
+	} finally {
+		// hide the conversion bar
+		conversionBar.hide();
 	}
-	await vscode.commands.executeCommand('workbench.action.closeOtherEditors');
 }
