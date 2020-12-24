a2378dc537367c2b773bb75477cf58e90012c94f
Yankun Huang <yankhuan@microsoft.com>
2020-12-24 15:23:25 +0800

    Update engine (3.1.2)

--- FHIR-Converter/client/src/core/common/constants/template-management.ts
+++ FHIR-Converter/client/src/core/common/constants/template-management.ts
@@ -7,5 +7,5 @@ import * as path from 'path';
 
 export const DefaultTemplateManagementExePath = path.join(__dirname, '../../../../engine/Microsoft.Health.Fhir.Liquid.Converter.Tool.exe');
 export const DefaultOrasExePath = path.join(__dirname, '../../../../engine/oras.exe');
-export const OfficialImageReference = 'healthplatformregistry.azurecr.io/hl7v2defaulttemplates:v3.1.1'; 
-export const SampleDataReference = 'healthplatformregistry.azurecr.io/hl7v2sampledata:v3.1.1';
+export const OfficialImageReference = 'healthplatformregistry.azurecr.io/hl7v2defaulttemplates:v3.1.2'; 
+export const SampleDataReference = 'healthplatformregistry.azurecr.io/hl7v2sampledata:v3.1.2';
