07f4ac503919c4ea2a0a67b93698d23236d6cc1d
Yankun Huang <yankhuan@microsoft.com>
2021-06-01 13:16:44 +0800

    Fix unit tests

--- FHIR-Converter/client/src/test/suite/index.ts
+++ FHIR-Converter/client/src/test/suite/index.ts
@@ -48,7 +48,7 @@ export async function run(): Promise<void> {
 	runner.useColors(true);
 	runner.addFile(join(__dirname, './view/extension.test'));
 	runner.addFile(join(__dirname, './converter/converter.test'));
-	runner.addFile(join(__dirname, './converter/engine/hl7v2-converter-engine.test'));
+	runner.addFile(join(__dirname, './converter/engine/fhir-converter-engine.test'));
 
 	const options = { cwd: __dirname };
 	const files = glob.sync('**/*utils.test.js', options);
