1638f7f5d0ac6fd78be3e3c9915f135f7f325e94
Yankun Huang <yankhuan@microsoft.com>
2021-07-22 21:06:21 +0800

    Add script to install engine

---
+++ FHIR-Converter/client/install-engine.sh
@@ -0,0 +1,14 @@
+version="4.0.102.2"
+
+if [ ! -d "engine-tmp" ]; then
+    mkdir ./engine-tmp
+fi
+curl --location --request GET "https://pkgs.dev.azure.com/microsofthealthoss/FhirConverter/_apis/packaging/feeds/FhirConverterPublic/nuget/packages/Microsoft.Health.Fhir.Liquid.Converter.Tool/versions/${version}/content?api-version=6.0-preview.1" -o ./engine-tmp/engine.nupkg
+unzip -o ./engine-tmp/engine.nupkg -d ./engine-tmp
+if [ ! -d "engine" ]; then
+    mkdir ./engine
+else
+    rm -r ./engine/*
+fi
+mv ./engine-tmp/contentFiles/any/netcoreapp**/Microsoft.Health.Fhir.Liquid.Converter.Tool/* ./engine/
+rm -r engine-tmp
